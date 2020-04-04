16aecd79341b78c3c4302a446d4e046e43e5eac6
IllescasJB <jovani_bryan@hotmail.com>
2020-04-04 00:18:36 -0600

    Borrar entorno virtual

--- myproject/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/mac/Documents/adoo/djangoproject/adtomin/myproject/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
