2aab96ceb88a01233384e36a74d5b70e7930d158
IllescasJB <jovani_bryan@hotmail.com>
2020-04-03 22:47:27 -0600

    Modificación bd sqlite

--- adtomin/settings.py
+++ adtomin/settings.py
@@ -80,14 +80,21 @@ WSGI_APPLICATION = 'adtomin.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'adtominTest',
+#        'USER': 'postgres',
+#        'PASSWORD': 'postgres',
+#        'HOST': '',
+#        'PORT': '',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'adtominTest',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
